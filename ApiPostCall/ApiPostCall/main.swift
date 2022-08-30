61a559618a16c7abc2cbba6f68841e3654c5df61
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-08-30 11:13:23 +0300

    ApiCall

--- ApiPostCall/ApiPostCall/main.swift
+++ ApiPostCall/ApiPostCall/main.swift
@@ -1,11 +1,53 @@
-//
-//  main.swift
-//  ApiPostCall
-//
-//  Created by Valentina Krysak on 30.08.22.
-//
 
 import Foundation
 
-print("Hello, World!")
+    // GET, POST, DELETE, etc.
+
+func makePOSTRequest() {
+    guard let  url  = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+        return
+    }
+
+    print("Making api call...")
+
+    var request = URLRequest(url: url)
+        // method, body, headers
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    let body: [String: AnyHashable] = [
+        "userId": 1,
+        "title": "Hello From IOS Academy",
+        "body": "The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog."
+    ]
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+
+        // Make the request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+
+        do {
+            let response = try JSONDecoder().decode(Response.self, from: data)
+            print("SUCCESS: \(response)")
+        }
+        catch {
+            print(error)
+        }
+    }
+
+    task.resume()
+
+
+}
+
+struct Response: Codable {
+    let body: String
+    let id: Int
+    let userId: Int
+    let title: String
+}
+
+let post = makePOSTRequest()
+print(post)
 
