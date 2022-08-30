91deb5f88baeed07f4381d13cc94b68c900a2a34
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-08-30 10:38:34 +0300

    Add files via upload

---
+++ PostAPI/PostAPI/PostAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAPIApp.swift
+//  PostAPI
+//
+//  Created by Valentina Krysak on 30.08.22.
+//
+
+import SwiftUI
+
+@main
+struct PostAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
